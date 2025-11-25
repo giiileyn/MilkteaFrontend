6067c083c019d4b662a2f2be419723e1fdb9e4ba
Gelain <gcastronuevo026@gmail.com>
2025-11-25 22:08:51 +0800

    link

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+
 
 const Login = () => {
   const navigate = useNavigate();
